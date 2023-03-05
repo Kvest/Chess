c2c1a25d3c03c59d0537bf4ab58a97ee9b8ae7bf
Roman Bielokon <belokon.roman@gmail.com>
2023-03-05 02:25:43 +0100

    adjust UI according to orientation

--- app/src/main/java/com/kvest/chess/ui/chess_game/ChessGameScreen.kt
+++ app/src/main/java/com/kvest/chess/ui/chess_game/ChessGameScreen.kt
@@ -1,18 +1,19 @@
 package com.kvest.chess.ui.chess_game
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.kvest.chess.R
 import com.kvest.chess.model.*
 import com.kvest.chess.ui.chess_board.ChessBoard
 import com.kvest.chess.ui.chess_board.ChessBoardListener
@@ -33,25 +34,53 @@ fun ChessGameScreen(
     game: ChessGameUIState,
     listener: ChessBoardListener,
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(top = 16.dp)
-    ) {
-        ChessBoard(
-            chessBoard = game.board,
-            pieces = game.pieces,
-            selectedSquare = game.selectedSquare,
-            squaresForMove = game.squaresForMove,
-            promotions = game.promotions,
-            squareSize = dimensionResource(R.dimen.square_size),
-            listener = listener,
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-        )
-        GameHistory(
-            history = game.history,
+    if (LocalConfiguration.current.orientation == Configuration.ORIENTATION_PORTRAIT) {
+        Column(
             modifier = Modifier
-                .padding(top = 20.dp)
-        )
+                .fillMaxSize()
+                .padding(top = 16.dp)
+        ) {
+            val squareSize = ((LocalConfiguration.current.screenWidthDp - 16) / 8).dp
+
+            ChessBoard(
+                chessBoard = game.board,
+                pieces = game.pieces,
+                selectedSquare = game.selectedSquare,
+                squaresForMove = game.squaresForMove,
+                promotions = game.promotions,
+                squareSize = squareSize,
+                listener = listener,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+            GameHistory(
+                history = game.history,
+                modifier = Modifier
+                    .padding(top = 20.dp, start = 8.dp, end = 8.dp)
+            )
+        }
+    } else {
+        val squareSize = ((LocalConfiguration.current.screenHeightDp - 16) / 8).dp
+
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 8.dp)
+        ) {
+            ChessBoard(
+                chessBoard = game.board,
+                pieces = game.pieces,
+                selectedSquare = game.selectedSquare,
+                squaresForMove = game.squaresForMove,
+                promotions = game.promotions,
+                squareSize = squareSize,
+                listener = listener,
+                modifier = Modifier.align(Alignment.CenterVertically)
+            )
+            GameHistory(
+                history = game.history,
+                modifier = Modifier
+                    .padding(top = 8.dp, start = 20.dp, end = 8.dp)
+            )
+        }
     }
 }
